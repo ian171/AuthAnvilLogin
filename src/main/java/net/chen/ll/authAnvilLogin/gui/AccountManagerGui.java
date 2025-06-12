98dfb6daaa103aa73145e0497cc645e7bdbfc507
Chen <3623813757@qq.com>
2025-06-12 22:54:28 +0800

    更新了账户管理gui界面

--- src/main/java/net/chen/ll/authAnvilLogin/gui/AccountManagerGui.java
+++ src/main/java/net/chen/ll/authAnvilLogin/gui/AccountManagerGui.java
@@ -3,6 +3,9 @@ package net.chen.ll.authAnvilLogin.gui;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -10,14 +13,27 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AccountManagerGui {
+public class AccountManagerGui implements Listener {
+    public static Inventory inv;
     public static void open(Player player) {
         Inventory inventory = Bukkit.createInventory(player, 54, "账户管理");
+        inv= inventory;
         ItemStack logout = new ItemStack(Material.REDSTONE);
         ItemMeta logoutMeta = logout.getItemMeta();
         addItem(inventory, Material.REDSTONE, logoutMeta,"注销登录", null,3);
         addItem(inventory,Material.IRON_INGOT,"删除账户",null,5);
         player.openInventory(inventory);
+    }
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        Player p = (Player) event.getWhoClicked();
+        if (event.getInventory() == inv) {
+            if (event.getCurrentItem() == null || event.getCurrentItem().getType().equals(Material.REDSTONE)) {
+                p.performCommand("logout");
+            }else if(event.getCurrentItem().getType().equals(Material.IRON_INGOT)){
+                p.performCommand("unregister");
+            }
+        }
     }
     private static void addItem(Inventory inventory, Material material,ItemMeta itemMeta, String name, List<String> lore,int index) {
         ItemStack item = new ItemStack(material);
