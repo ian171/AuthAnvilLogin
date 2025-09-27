38ce7bc72e7d9260b9b9490404c4e5b0041b0096
chen <3623813757@qq.com>
2025-09-27 16:53:33 +0800

    v1.2.5-Update 升级自动检测更新
    Signed-off-by: chen <3623813757@qq.com>

--- src/main/java/net/chen/ll/authAnvilLogin/AuthAnvilLogin.java
+++ src/main/java/net/chen/ll/authAnvilLogin/AuthAnvilLogin.java
@@ -17,8 +17,6 @@ import org.jetbrains.annotations.NotNull;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
